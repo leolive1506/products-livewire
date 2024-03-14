6f377427145c708c9f25cac8695e94fcf40ccaca
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-14 14:15:20 -0300

    feat: create icon test

--- tests/Feature/Livewire/Customer/Product/Cart/IconTest.php
+++ tests/Feature/Livewire/Customer/Product/Cart/IconTest.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Livewire\Customer\Product\Cart\Icon;
+use App\Models\Product;
+use App\Models\User;
 use Livewire\Livewire;
 
-it('renders successfully', function () {
-    Livewire::test(Icon::class)
-        ->assertStatus(200);
+it('update quantity cart', function () {
+    $product = Product::factory()->createOne();
+    $product2 = Product::factory()->createOne();
+    $user = User::factory()->createOne();
+
+    $livewire = Livewire::actingAs($user)->test(Icon::class);
+    $livewire->dispatch('add-product-to-cart', $product)
+        ->assertSee("1");
+    $livewire->dispatch('add-product-to-cart', $product2)
+        ->assertSee("2");
 });
--- tests/Feature/Livewire/Customer/Product/IndexTest.php
+++ tests/Feature/Livewire/Customer/Product/IndexTest.php
@@ -31,3 +31,26 @@ it('add product to cart and dispatch buy-product event', function () {
         'quantity' => 1
     ]);
 });
+
+it('update quantity product quantity to cart when dispatch buy-product event', function () {
+    $product = Product::factory()->createOne();
+    $user = User::factory()->createOne();
+
+    $livewire = Livewire::actingAs($user)
+        ->test(Index::class)
+        ->call('buy', $product)
+        ->assertDispatched('add-product-to-cart');
+
+    $this->assertDatabaseHas('carts', [
+        'product_id' => $product->id,
+        'user_id' => $user->id,
+        'quantity' => 1
+    ]);
+
+    $livewire->call('buy', $product)->assertDispatched('add-product-to-cart');
+    $this->assertDatabaseHas('carts', [
+        'product_id' => $product->id,
+        'user_id' => $user->id,
+        'quantity' => 2
+    ]);
+});

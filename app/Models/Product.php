4e7ac3259a6b5bfe9df592cbf0c7a7c274b1956b
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-14 11:14:31 -0300

    feat: create product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserRole extends Model
+class Product extends Model
 {
     use HasFactory;
-    protected $guarded = ['id'];
 
-    public static $roles = [
-        'ADMIN',
-        'CUSTOMER'
-    ];
+    protected $guarded = ['id'];
 }
+
